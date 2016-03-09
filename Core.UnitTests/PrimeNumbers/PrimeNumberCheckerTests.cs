6a81395c2d279a71b56c358075ebd5182e3623b3
Cem Ozturan <cemozturan@gmail.com>
2016-03-09 22:45:17 +0000

    -- Adding initial test for the class to check if a number is prime or not -- Test that anything smaller than 2 is not a prime

---
+++ Core.UnitTests/PrimeNumbers/PrimeNumberCheckerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Core.UnitTests.PrimeNumbers
+{
+	[TestFixture]
+	public class PrimeNumberCheckerTests
+	{
+		private PrimeNumberChecker _primeNumberChecker;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_primeNumberChecker = new PrimeNumberChecker();
+		}
+
+		[TestCase(1)]
+		[TestCase(0)]
+		[TestCase(-1)]
+		public void NumbersSmallerThan2ShouldFail(int number)
+		{
+			// Arrange
+			// Act
+			var isPrime = _primeNumberChecker.IsNumberPrime(number);
+
+			// Assert
+			Assert.IsFalse(isPrime);
+		}
+	}
+}
