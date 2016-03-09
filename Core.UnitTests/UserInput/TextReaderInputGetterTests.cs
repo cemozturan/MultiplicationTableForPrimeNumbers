e13a780bd9ab8dc0da69b1a80c41f635fca1a60c
Cem Ozturan <cemozturan@gmail.com>
2016-03-09 21:05:33 +0000

    -- Adding tests to read strings with no line breaks

--- Core.UnitTests/UserInput/TextReaderInputGetterTests.cs
+++ Core.UnitTests/UserInput/TextReaderInputGetterTests.cs
@@ -22,10 +22,24 @@ namespace Core.UnitTests.UserInput
             var stringReader = new StringReader(string.Empty);
             
             // Act
-            var input = _textReaderInputGetter.GetInput(stringReader);
+            var inputRead = _textReaderInputGetter.GetInput(stringReader);
 
             // Assert
-            Assert.IsTrue(string.IsNullOrEmpty(input));
+            Assert.IsTrue(string.IsNullOrEmpty(inputRead));
+        }
+
+        [TestCase("some string with no line breaks")]
+        [TestCase("NoWhiteSpaceAndSomeNumbersShouldBeRead123456")]
+        public void StringsWithNoLineBreakShouldBeRead(string userInput)
+        {
+            // Arrange
+            var stringReader = new StringReader(userInput);
+
+            // Act
+            var inputRead = _textReaderInputGetter.GetInput(stringReader);
+
+            // Assert
+            Assert.AreEqual(userInput, inputRead);
         }
     }
 }
