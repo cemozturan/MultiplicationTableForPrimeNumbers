ef1c30817c10ea2651540f44a7299b4b28c6c431
Cem Ozturan <cemozturan@gmail.com>
2016-03-09 21:27:14 +0000

    -- Adding initial test file for input validation -- Non-integer inputs should fail

---
+++ Core.UnitTests/UserInput/InputValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Core.UnitTests.UserInput
+{
+    [TestFixture]
+    public class InputValidatorTests
+    {
+        private InputValidator _inputValidator;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_inputValidator = new InputValidator();
+		}
+
+		[TestCase("")]
+		[TestCase("someText")]
+		[TestCase("2.7")]
+		[TestCase("4/2")]
+		public void NonIntegerInputShouldFail(string userInput)
+		{
+			// Arrange
+			// Act
+			var isValid = _inputValidator.ValidateInput(userInput);
+
+			// Assert
+			Assert.IsFalse(isValid);
+		}
+    }
+}
