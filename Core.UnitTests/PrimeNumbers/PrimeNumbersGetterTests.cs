a653beccfe42556fe7b9dbea3cd3ee91d275e766
Cem Ozturan <cemozturan@gmail.com>
2016-03-09 22:20:02 +0000

    -- Adding initial tests for getting prime numbers -- An exception should be thrown if the number of primes asked for is not a positive integer

---
+++ Core.UnitTests/PrimeNumbers/PrimeNumbersGetterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Core.UnitTests.PrimeNumbers
+{
+	[TestFixture]
+	public class PrimeNumbersGetterTests
+	{
+		private PrimeNumbersGetter _primeNumbersGetter;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_primeNumbersGetter = new PrimeNumbersGetter();
+		}
+
+		[TestCase(0)]
+		[TestCase(-1)]
+		public void NonPositiveIntegersShouldThrowException(int numberOfPrimes)
+		{
+			Assert.Throws<ArgumentException>(
+				_primeNumbersGetter.GetFirstNPrimeNumbers(numberOfPrimes));
+		}
+	}
+}
