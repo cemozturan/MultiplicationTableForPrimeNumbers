bbf176f0b5afa87e6fce8bd6d0894fdbe473565c
Cem Ozturan <cemozturan@gmail.com>
2016-03-09 20:21:42 +0000

    -- Adding the initial unit test file for reading user input

---
+++ Core.UnitTests/UserInput/TextReaderInputGetterTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Core.UnitTests.UserInput
+{
+    [TestFixture]
+    class TextReaderInputGetterTests
+    {
+        private IUserInputGetter _textReaderInputGetter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _textReaderInputGetter = new TextReaderInputGetter();
+        }
+    }
+}
