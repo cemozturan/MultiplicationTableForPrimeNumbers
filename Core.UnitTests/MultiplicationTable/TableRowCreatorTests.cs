a9285066595167bde2796a4a4a1740b3e6d19ff5
Cem Ozturan <cemozturan@gmail.com>
2016-03-10 01:39:36 +0000

    -- Adding test to check that first row contains all prime numbers found -- Adding tests to check that each row other than the first row starts with the corresponding prime number

--- Core.UnitTests/MultiplicationTable/TableRowCreatorTests.cs
+++ Core.UnitTests/MultiplicationTable/TableRowCreatorTests.cs
@@ -27,5 +27,36 @@ namespace Core.UnitTests.MultiplicationTable
 			// Assert
 			Assert.AreEqual("|   | " + primeNumber + " |", row);
 		}
+
+		[Test]
+		public void FirstRowShouldContainAllFoundPrimeNumbers()
+		{
+			// Arrange
+			var primes = new int[3] { 2, 3, 5 };
+
+			// Act
+			var row = _tableRowCreator.CreateRow(primes, null);
+
+			// Assert
+			Assert.AreEqual("|   | " + primes[0] + " | "
+				+ primes[1] + " | "
+				+ primes[2] + " |",
+				row);
+		}
+
+		[TestCase(0)]
+		[TestCase(1)]
+		[TestCase(2)]
+		public void RowsOtherThanTheFirstRowShouldStartWithTheCorrespondingPrimeNumber(int indexOfPrime)
+		{
+			// Arrange
+			var primes = new int[3] { 2, 3, 5 };
+
+			// Act
+			var row = _tableRowCreator.CreateRow(primes, primes[indexOfPrime]);
+
+			// Assert
+			Assert.IsTrue(row.StartsWith("| " + primes[indexOfPrime] + " |"));
+		}
 	}
 }
